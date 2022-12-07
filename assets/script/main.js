8ded4ae94a403e64036e1adf0a6586f38fc353bd
Hoàng An <20521051@gm.uit.edu.vn>
2022-12-07 11:57:53 +0700

    20521051
    test

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,26 +1,51 @@
-const sidebar = document.querySelector('.sidebar');
-const navItems = document.querySelectorAll('nav .nav-item');
-const toggle = document.querySelector('.sidebar .toggle');
+/* navbar */
+const body = document.querySelector('body'),
+      sidebar = body.querySelector('nav'),
+      toggle = body.querySelector(".toggle"),
+      searchBtn = body.querySelector(".search-box"),
+      modeSwitch = body.querySelector(".toggle-switch"),
+      modeText = body.querySelector(".mode-text");
 
-toggle.addEventListener('click', () => {
 
-    if (sidebar.className === 'sidebar')
-        sidebar.classList.add('open');
-    else
-        sidebar.classList.remove('open');
+toggle.addEventListener("click" , () =>{
+    sidebar.classList.toggle("close");
+})
 
+searchBtn.addEventListener("click" , () =>{
+    sidebar.classList.remove("close");
+})
+
+modeSwitch.addEventListener("click" , () =>{
+    body.classList.toggle("dark");
+    
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
+        
+    }
 });
-
-navItems.forEach(navItem => {
-
-    navItem.addEventListener('click', () => {
-
-        navItems.forEach(navItem => {
-            navItem.classList.remove('active');
-        });
-
-        navItem.classList.add('active');
-
-    });
-
-});
+/* slide show */
+var slideIndex = 1;
+ showSlides(slideIndex);
+function plusSlides(n) {
+   showSlides(slideIndex += n);
+ }
+function currentSlide(n) {
+   showSlides(slideIndex = n);
+ }
+function showSlides(n) {
+   var i;
+   var slides = document.getElementsByClassName("mySlides");
+   var dots = document.getElementsByClassName("dot");
+   if (n > slides.length) {slideIndex = 1}
+   if (n < 1) {slideIndex = slides.length}
+   for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";
+   }
+   for (i = 0; i < dots.length; i++) {
+       dots[i].className = dots[i].className.replace(" active", "");
+   }
+   slides[slideIndex-1].style.display = "block";
+   dots[slideIndex-1].className += " active";
+ }
